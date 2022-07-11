4011a217992afbdcefb96dc86760a1967ed2ced9
Dzmitry Bichko <dzmitry_bichko@epam.com>
2022-07-11 21:17:10 +0300

    fixed saned

--- sedawk.sh
+++ sedawk.sh
@@ -3,7 +3,7 @@
 #a
 cp passwd passwd_new
 #b
-awk 'BEGIN{OFS=FS=":"}$1=="saned"{$7="/usr/bash"}{print}' passwd_new > tmp && mv tmp passwd_new
+awk 'BEGIN{OFS=FS=":"}$1=="saned"{$7="/bin/bash"}{print}' passwd_new > tmp && mv tmp passwd_new
 #c
 sed -i '/avahi/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
 #d
@@ -11,5 +11,5 @@ awk 'BEGIN{FS=":"; OFS=":"} {print $1,$3,$5,$7}' passwd_new > tmp && mv tmp pass
 #e
 sed -i '/daemon/d' passwd_new
 #f
-awk 'BEGIN{OFS=FS=":"}$2%2==0{$7="/bin/zsh"}{print}' passwd_new > tmp && mv tmp passwd_new
+awk 'BEGIN{OFS=FS=":"}$2%2==0{$4="/bin/zsh"}{print}' passwd_new > tmp && mv tmp passwd_new
 	# Put you code here
