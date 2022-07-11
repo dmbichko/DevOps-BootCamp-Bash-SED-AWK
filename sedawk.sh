a8401a6dcb7bbb931ae49fece57418fb8788b2c1
Dzmitry Bichko <dzmitry_bichko@epam.com>
2022-07-11 13:45:01 +0300

    1st

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+#a
+cp passwd passwd_new
+#b
+awk 'BEGIN{OFS=FS=":"}$1=="saned"{$7="/usr/bash"}{print}' passwd_new > tmp && mv tmp passwd_new
+#c
+sed -i '/avahi/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
+#d
+awk 'BEGIN{FS=":"; OFS=":"} {print $1,$3,$5,$7}' passwd_new > tmp && mv tmp passwd_new
+#e
+sed -i '/daemon/d' passwd_new
+
+
 # Put you code here
